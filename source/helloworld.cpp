21053aea30b77a67482b8a3738a42d20cded7580
Vanessa Retz <vanessa.retz@aol.com>
2018-04-21 13:50:45 +0200

    ..

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -2,12 +2,14 @@
 #include <cmath>
 
 int binomial(int);
-int gcd(int , int );
+int gcd(int a, int b);
+int checksum(int);
 
 int main()
 {
   std::cout << "Hello, World2!\n";
   std::cout << binomial(6) << "\n";
+  std::cout << gcd(20,2) << "\n";
   bool found = false;
   int i = 0;
   while(!found){
@@ -28,22 +30,11 @@ int binomial(int p){
   return 5 + p;
 }
 
-int gcd(int a, int b){
-  int result;
-  if (a!=b){
-
-    while (a > 0 && b > 0){
-      if (a>b){
-        a = a&b;
-      }
-      else if (b > a){
-        b = b%a;
-      }
-    }
+int gcd ( int a , int b )
+  {
+    if(b == 0)
+        return a;
+    else return gcd(b, a % b);
   }
-if (a > 0) result = a;
-if (b > 0) result = b;
-if (a == b) result = a/b;
-  return result;
 
-}
+int checksum(int m){}}
