b06e484024b82b4645b5a062ac932ded7de3c82c
Vanessa Retz <vanessa.retz@aol.com>
2018-04-21 13:07:21 +0200

    1to20

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
+int binomial(int);
+
 int main()
 {
-  std::cout << "Hello, World!\n";
+  std::cout << "Hello, World2!\n";
+  std::cout << binomial(6) << "\n";
+  bool found = false;
+  int i = 0;
+  while(!found){
+      for(int c = 1; c < 21; c++){
+      found = true;
+      if(i == 0 || i % c != 0){
+        i += 20;
+        found = false;
+        break;
+      }
+    }
+  }
+  std::cout << i;
   return 0;
+}
+
+int binomial(int p){
+  return 5 + p;
 }
